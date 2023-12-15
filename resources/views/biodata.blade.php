43e69da799bf89c35b7a0c518436c7f46c77baef
RIZKIPUJI-SAMS\Rizki Puji Lestari <rizki@ekuator.id>
2023-12-15 09:09:24 +0700

    Initial commit - Hello World dan Biodata

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <title>Resume of Rizki Puji Lestari</title>
+    
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Responsive Resume Template">
+    <meta name="author" content="Xiaoying Riley at 3rd Wave Media">    
+    <link rel="shortcut icon" href="favicon.ico"> 
+    
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+    
+    <!-- FontAwesome JS-->
+	<script defer src="assets/fontawesome/js/all.min.js"></script>
+       
+    <!-- Theme CSS -->  
+    <link id="theme-style" rel="stylesheet" href="assets/css/pillar-1.css">
+
+
+</head> 
+
+<body>	
+
+    <article class="resume-wrapper text-center position-relative">
+	    <div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
+		    <header class="resume-header pt-4 pt-md-0">
+			    <div class="row">
+				    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
+				        <img class="picture" src="assets/images/profile2.jpg" alt="">
+				    </div><!--//col-->
+				    <div class="col">
+					    <div class="row p-4 justify-content-center justify-content-md-between">
+						    <div class="primary-info col-auto">
+							    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">Rizki Puji Lestari</h1>
+							    <div class="title mb-3">Full Stack Web Developer</div>
+							    <ul class="list-unstyled">
+								    <li class="mb-2">
+								    	<i class="far fa-envelope fa-fw me-2" data-fa-transform="grow-3"></i>rizkipuj@gmail.com
+								    </li>
+								    <li>
+								    	<i class="fas fa-mobile-alt fa-fw me-2" data-fa-transform="grow-6"></i>+62 813 2228 6797
+								    </li>
+							    </ul>
+						    </div><!--//primary-info-->
+						    <div class="secondary-info col-auto mt-2">
+							    <ul class="resume-social list-unstyled">
+					                <li class="mb-3"><a class="text-link" href="https://www.instagram.com/rizkiujilestari"><span class="fa-container text-center me-2"><i class="fab fa-instagram fa-fw"></i></span>@rizkiujilestari</a></li>
+					                <li class="mb-3"><a class="text-link" href="https://www.github.com/rizkipujilestari"><span class="fa-container text-center me-2"><i class="fab fa-github-alt fa-fw"></i></span>github.com/rizkipujilestari</a></li>
+					                <li class="mb-3"><a class="text-link" href="https://www.gitlab.com/rizki-puji"><span class="fa-container text-center me-2"><i class="fab fa-gitlab fa-fw"></i></span>gitlab.com/rizki-puji</a></li>
+							    </ul>
+						    </div><!--//secondary-info-->
+					    </div><!--//row-->
+					    
+				    </div><!--//col-->
+			    </div><!--//row-->
+		    </header>
+		    <div class="resume-body p-5">
+			    <section class="resume-section summary-section mb-5">
+				    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Career Summary</h2>
+				    <div class="resume-section-content">
+					    <p class="mb-0">Hello! I am Rizki Puji Lestari, a web programmer based in Bandung City. 
+					    	I have experience working as a part of a team and individually. 
+					    	I am also energetic and eager to learn new skills. 
+					    	I used to build a php program with framework CodeIgniter or Laravel or native php program. 
+					    	I also able to build a responsive web designs with css and javascript or jquery. 
+					    	I usually use Visual Studio Code or Sublime Text 3 as my source-code editors.
+						</p>
+				    </div>
+			    </section><!--//summary-section-->
+			    <div class="row">
+				    <div class="col-lg-9">
+					    <section class="resume-section experience-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Work Experience</h2>
+						    <div class="resume-section-content">
+							    <div class="resume-timeline position-relative">
+								    <article class="resume-timeline-item position-relative pb-5">
+									    
+									    <div class="resume-timeline-item-header mb-2">
+										    <div class="d-flex flex-column flex-md-row">
+										        <h3 class="resume-position-title font-weight-bold mb-1">Web Developer & Implementor (Fullstack)</h3>
+										        <div class="resume-company-name ms-auto">PT. Alfatih Solusi Technology, Sukabumi</div>
+										    </div><!--//row-->
+										    <div class="resume-position-time">July 2023 - Present</div>
+									    </div><!--//resume-timeline-item-header-->
+									    <div class="resume-timeline-item-desc">
+										    <p>Build feature of a Hospital Healthcare Web System called Medicare and make EMR (Electronic Medical Record) forms.</p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+										    <ul class="list-inline">
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">Laravel</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">Javascript</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">jQuery</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">HTML/SASS</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">SQL Server</span></li>
+										    </ul>
+									    </div><!--//resume-timeline-item-desc-->
+
+								    </article><!--//resume-timeline-item-->
+								    
+								    <article class="resume-timeline-item position-relative pb-5">
+									    
+									    <div class="resume-timeline-item-header mb-2">
+										    <div class="d-flex flex-column flex-md-row">
+										        <h3 class="resume-position-title font-weight-bold mb-1">Backend Web Programmer</h3>
+										        <div class="resume-company-name ms-auto">PT. Torche Indonesia, Bandung</div>
+										    </div><!--//row-->
+										    <div class="resume-position-time">March 2022 - June 2023</div>
+									    </div><!--//resume-timeline-item-header-->
+									    <div class="resume-timeline-item-desc">
+										    <p>1. Build feature of a goverment organization website</p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+										    <ul class="list-inline">
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">Lumen</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">PostgreSQL</span></li>
+										    </ul>
+										    <p>2. Build a fingerprint desktop application</p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+										    <ul class="list-inline">
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">C#</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">PostgreSQL</span></li>
+										    </ul>
+									    </div><!--//resume-timeline-item-desc-->
+
+								    </article><!--//resume-timeline-item-->
+								    
+								    <article class="resume-timeline-item position-relative">
+									    
+									    <div class="resume-timeline-item-header mb-2">
+										    <div class="d-flex flex-column flex-md-row">
+										        <h3 class="resume-position-title font-weight-bold mb-1">Frontend Web Programmer</h3>
+										        <div class="resume-company-name ms-auto">PT Akses Cipta Solusi, Bandung</div>
+										    </div><!--//row-->
+										    <div class="resume-position-time">Sep 2019 - Feb 2022</div>
+									    </div><!--//resume-timeline-item-header-->
+									    <div class="resume-timeline-item-desc">
+										    <p>1. Build a healtcare web design</p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+										    <ul class="list-inline">
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">CodeIgniter</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">JavaScript</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">CSS</span></li>
+										    </ul>
+										    <p>2. Build feature of a clinic healtcare web system named SEHATI</p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+										    <ul class="list-inline">
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">CodeIgniter</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">JavaScript</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">CSS</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">PostgreSQL</span></li>
+										    </ul>
+										    <p>3. Build a web company profile that integrated with API and CMS DRetail </p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+										    <ul class="list-inline">
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">Laravel</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">JavaScript</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">CSS</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">Rest API</span></li>
+										    </ul>
+									    </div><!--//resume-timeline-item-desc-->
+
+								    </article><!--//resume-timeline-item-->
+								
+							    </div><!--//resume-timeline-->							    
+						    </div>
+					    </section><!--//experience-section-->
+
+					    <section class="resume-section experience-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Education</h2>
+						    <div class="resume-section-content">
+							    <div class="resume-timeline position-relative">
+								    <article class="resume-timeline-item position-relative pb-5">
+									    <div class="resume-timeline-item-header mb-2">
+										    <div class="d-flex flex-column flex-md-row">
+										        <h3 class="resume-position-title font-weight-bold mb-1">Bachelor Degree of Informatics Engineering Major <br> Universitas Nurtanio Bandung</h3>
+										        <div class="resume-company-name ms-auto">Bandung</div>
+										    </div><!--//row-->
+										    <div class="resume-position-time">2021 – now (Evening Class)</div>
+									    </div><!--//resume-timeline-item-header-->
+								    </article><!--//resume-timeline-item-->
+
+								    <article class="resume-timeline-item position-relative">
+									    <div class="resume-timeline-item-header mb-2">
+										    <div class="d-flex flex-column flex-md-row">
+										        <h3 class="resume-position-title font-weight-bold mb-1">Vocational School Software Engineering Major <br> SMK Negeri 2 Sumedang</h3>
+										        <div class="resume-company-name ms-auto">Sumedang</div>
+										    </div><!--//row-->
+										    <div class="resume-position-time">2011 – 2014</div>
+									    </div><!--//resume-timeline-item-header-->
+									    <div class="resume-timeline-item-desc">
+										    GPA 8.6 (out of 10)
+									    </div><!--//resume-timeline-item-desc-->
+								    </article><!--//resume-timeline-item-->
+								
+							    </div><!--//resume-timeline-->							    
+						    </div>
+					    </section>
+				    </div>
+				    <div class="col-lg-3">
+					    <section class="resume-section skills-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Skills &amp; Tools</h2>
+						    <div class="resume-section-content">
+						        <div class="resume-skill-item">
+							        <h4 class="resume-skills-cat font-weight-bold">Frontend</h4>
+							        <ul class="list-unstyled mb-4">
+								        <li class="mb-2">
+								            <div class="resume-skill-name">HTML/CSS</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 96%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">JavaScript</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 90%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+							        </ul>
+						        </div><!--//resume-skill-item-->
+						        
+						        <div class="resume-skill-item">
+						            <h4 class="resume-skills-cat font-weight-bold">Backend</h4>
+							        <ul class="list-unstyled">
+								        <li class="mb-2">
+								            <div class="resume-skill-name">PHP</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 95%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">CodeIgniter</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 90%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">Lumen/Laravel</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 90%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">Rest API</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 85%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+							        </ul>
+						        </div><!--//resume-skill-item-->
+						        
+						        <div class="resume-skill-item">
+						            <h4 class="resume-skills-cat font-weight-bold">Others</h4>
+						            <ul class="list-inline">
+							            <li class="list-inline-item"><span class="badge badge-light">Git</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">UI/UX</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Code Review</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">RestAPI</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Regression Testing</span></li>
+						            </ul>
+						        </div><!--//resume-skill-item-->
+						    </div><!--resume-section-content-->
+					    </section><!--//skills-section-->
+					    
+					    <section class="resume-section reference-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Awards</h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled resume-awards-list">
+								    <li class="mb-2 ps-4 position-relative">
+								        <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
+								        <div class="resume-award-name">Beasiswa BOS</div>
+								        <div class="resume-award-desc">SMKN 2 Sumedang - 2012</div>
+								    </li>
+							    </ul>
+						    </div>
+					    </section><!--//interests-section-->
+
+					    <section class="resume-section language-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Language</h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled resume-lang-list">
+								    <li class="mb-2"><span class="resume-lang-name font-weight-bold">Indonesian</span> <small class="text-muted font-weight-normal">(Native)</small></li>
+								    <li class="mb-2 align-middle"><span class="resume-lang-name font-weight-bold">English</span> <small class="text-muted font-weight-normal">(Professional)</small></li>
+								    <li><span class="resume-lang-name font-weight-bold">Korean</span> <small class="text-muted font-weight-normal">(Basic Conversation)</small></li>
+							    </ul>
+						    </div>
+					    </section><!--//language-section-->
+					    <section class="resume-section interests-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Interests</h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled">
+								    <li class="mb-1">Photography</li>
+								    <li class="mb-1">Design</li>
+								    <li class="mb-1">Movies</li>
+							    </ul>
+						    </div>
+					    </section><!--//interests-section-->
+					    
+				    </div>
+			    </div><!--//row-->
+		    </div><!--//resume-body-->
+		    
+		    
+	    </div>
+    </article> 
+
+    
+    <footer class="footer text-center pt-2 pb-5">
+	    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
+        <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> by <a href="http://themes.3rdwavemedia.com" target="_blank">Xiaoying Riley</a> for developers</small>
+    </footer>
+
+    
+
+</body>
+</html> 
+
